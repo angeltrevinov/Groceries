f70b6d7f228fd3800b46acec91ca78d652548752
Angel Odiel Trevino Villanueva <angel.trevinov@gmail.com>
2019-03-19 19:49:40 -0600

    Testing css

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,12 +3,15 @@ import { Component } from "@angular/core";
 @Component({
     selector: "gr-main",
     template: `
-        <TextField hint="Email Address" keyboardType="email"
-         autocorrect="false" autocapitalizationType="none"></TextField>
-        <TextField hint="Password" secure="true"></TextField>
+        <StackLayout>
+            <TextField hint="Email Address" keyboardType="email"
+            autocorrect="false" autocapitalizationType="none"></TextField>
+            <TextField hint="Password" secure="true"></TextField>
 
-        <Button text="Sign in"></Button>
-        <Button text="Sign up for Groceries"></Button>
-    `
+            <Button text="Sign in" class="submit-button"></Button>
+            <Button text="Sign up for Groceries"></Button>
+        </StackLayout>
+    `,
+    styleUrls: ["app/pages/login/login-common.css","app/pages/login/login.css"]
 })
 export class AppComponent { }
