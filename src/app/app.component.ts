9117718d4290aaa8ea4d2a88c4010f39e04b2916
Angel Treviño <angel.trevinov@gmail.com>
2019-03-20 00:16:37 -0600

    Image does not work and i dont know why... I wanna sleep

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,6 +4,8 @@ import { Component } from "@angular/core";
     selector: "gr-main",
     template: `
         <StackLayout>
+        <Image src="res://logo_login" stretch="none" horizontalAlignment="center"></Image>
+            
             <TextField hint="Email Address" keyboardType="email"
             autocorrect="false" autocapitalizationType="none"></TextField>
             <TextField hint="Password" secure="true"></TextField>
@@ -12,6 +14,9 @@ import { Component } from "@angular/core";
             <Button text="Sign up for Groceries"></Button>
         </StackLayout>
     `,
-    styleUrls: ["app/pages/login/login-common.css","app/pages/login/login.css"]
+    styleUrls: [
+        "app/pages/login/login-common.css",
+        "app/pages/login/login.css"
+    ]
 })
 export class AppComponent { }
