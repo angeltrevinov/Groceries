5aff66fb590a4a3b9374f7b2a295951b0ed0f5cf
Angel Treviño <angel.trevinov@gmail.com>
2019-03-22 22:54:34 -0600

    deleting dumb files

--- platforms/ios/build/Groceries.build/Debug-iphonesimulator/Groceries.build/Objects-normal/x86_64/NativeScriptEmbedder.d
+++
@@ -1,4 +0,0 @@
-dependencies: \
-  /Users/angel/Documents/Personal\ Projects/Groceries/platforms/ios/Groceries/Groceries-Prefix.pch \
-  /Users/angel/Documents/Personal\ Projects/Groceries/node_modules/tns-core-modules/platforms/ios/src/NativeScriptEmbedder.m \
-  /Users/angel/Documents/Personal\ Projects/Groceries/node_modules/tns-core-modules/platforms/ios/src/NativeScriptEmbedder.h
